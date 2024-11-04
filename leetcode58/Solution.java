b8fb0144ab0135e63585287a02c086a499dcb3ec
nithruvan21 <97077809+nithruvan21@users.noreply.github.com>
2024-11-04 22:09:10 +0530

    58. Length of Last Word

---
+++ leetcode58/Solution.java
@@ -0,0 +1,42 @@
+// 58. Length of Last Word
+// Solved
+// Easy
+// Topics
+// Companies
+// Given a string s consisting of words and spaces, return the length of the last word in the string.
+
+// A word is a maximal 
+// substring
+//  consisting of non-space characters only.
+
+ 
+
+// Example 1:
+
+// Input: s = "Hello World"
+// Output: 5
+// Explanation: The last word is "World" with length 5.
+// Example 2:
+
+// Input: s = "   fly me   to   the moon  "
+// Output: 4
+// Explanation: The last word is "moon" with length 4.
+// Example 3:
+
+// Input: s = "luffy is still joyboy"
+// Output: 6
+// Explanation: The last word is "joyboy" with length 6.
+
+class Solution {
+    public static int lengthOfLastWord(String s) {
+        String[] str = s.split(" ");
+        int len = 0;
+        for(String s1: str){
+            len = s1.length();
+        } 
+        return len;
+    }
+    public static void main(String[] args) {
+        System.out.println(lengthOfLastWord("   fly me   to   the moon  "));
+    }
+}
