15b2954ab1732eb4f803b4a8aec5fda1b91e8c92
nithruvan21 <97077809+nithruvan21@users.noreply.github.com>
2024-12-02 22:51:23 +0530

    206. Reverse Linked List

---
+++ leetcode206/Solution.java
@@ -0,0 +1,48 @@
+// 206. Reverse Linked List
+// Solved
+// Easy
+// Topics
+// Companies
+// Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+ 
+
+// Example 1:
+
+
+// Input: head = [1,2,3,4,5]
+// Output: [5,4,3,2,1]
+// Example 2:
+
+
+// Input: head = [1,2]
+// Output: [2,1]
+// Example 3:
+
+// Input: head = []
+// Output: []
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode next = null;
+        ListNode prev = null;
+        ListNode cur = head;
+        while(cur!=null){
+            next = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = next;
+        }
+        return prev;
+    }
+}
